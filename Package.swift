4dae506a292ac56f4f17a403c41e25b5f9b9ecbb
Dongbin Kim <odong@imgbase.me>
2023-08-23 16:49:37 +0900

    📦 Package.swift 추가

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftyMarkdown",
+    products: [
+        .library(name: "SwiftyMarkdown", targets: ["SwiftyMarkdown"])
+    ],
+    targets: [
+        .target(name: "SwiftyMarkdown", dependencies: [])
+    ]
+)
